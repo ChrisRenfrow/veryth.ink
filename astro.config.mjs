d952c84ec83118c28fef525f68b9925f1e2410f8
Chris Renfrow <dev@chrisrenfrow.me>
2024-09-06 18:29:43 -0700

    Redirect base path for blog, categories, tags to respective first page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,18 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   markdown: {
-    remarkPlugins: [/* Make/install read time plugin */]
-  }
-});
+    remarkPlugins: [
+      /* Make/install read time plugin */
+    ],
+  },
+  redirects: {
+    '/blog': '/blog/1',
+    '/blog/categories/[category]': '/blog/categories/[category]/1',
+    '/blog/tags/[tag]': '/blog/tags/[tag]/1',
+  },
+})

018e1d0089a2743ecf26829620dcc78f35d11a42
Chris Renfrow <dev@chrisrenfrow.me>
2024-09-15 11:25:16 -0700

    Refactor due to improvements and additions to queries.js

--- src/content/config.ts
+++ src/content/config.ts
@@ -18,6 +18,36 @@ const blogCollection = defineCollection({
   }),
 })
 
+export const projectSchema = z.object({
+  name: z.string(),
+  description: z.string(),
+  homepage: z.string().url(),
+  repository: z.optional(z.string().url()),
+  status: z.enum(['active', 'on-hold', 'complete', 'archived']),
+  draft: z.optional(z.boolean()),
+  license: z.optional(z.enum(['MIT', 'Apache-2.0', 'GPL-3.0'])),
+  started_on: z.date(),
+  finished_on: z.optional(z.date()),
+  associated_tags: z.optional(z.array(z.string())),
+  contributors: z.optional(
+    z.array(
+      z.object({
+        name: z.string(),
+        profile_url: z.optional(z.string().url()),
+        email: z.optional(z.string().email()),
+      }),
+    ),
+  ),
+})
+
+export type ProjectSchema = z.infer<typeof projectSchema>
+
+const projectCollection = defineCollection({
+  type: 'content',
+  schema: projectSchema,
+})
+
 export const collections = {
   blog: blogCollection,
+  projects: projectCollection,
 }
--- src/helpers/queries.js
+++ src/helpers/queries.js
@@ -1,31 +1,71 @@
 import { getCollection } from 'astro:content'
+import { slugify } from './utils'
 
-export async function getAllPosts(
+const PROD = import.meta.env.NETLIFY
+  ? import.meta.env.CONTEXT == 'production'
+  : import.meta.env.PROD
+
+export async function getPosts(options) {
   options = {
-    chronSort: true,
-    includeDrafts: false,
-  },
-) {
-  let posts = await getCollection('blog')
-  posts = !options.includeDrafts
-    ? posts.filter((post) => !post.data.draft)
-    : posts
+    ...{
+      chronSort: true,
+      tags: null,
+      category: null,
+    },
+    ...options,
+  }
+  let posts = await getCollection('blog', ({ data }) => {
+    let includePost = PROD ? data.draft !== true : true
+    if (options.tags && includePost) {
+      includePost = options.tags.some(
+        (tag) => data.tags && data.tags.includes(tag),
+      )
+    }
+    if (options.category && includePost) {
+      includePost = data.category === options.category
+    }
+    return includePost
+  })
   if (options.chronSort)
     posts.sort(
       (a, b) => b.data.published_on.valueOf() - a.data.published_on.valueOf(),
     )
   return posts
 }
+export async function getTags() {
+  const posts = await getPosts({ chronSort: false })
+  const tagMap = new Map()
+  posts.forEach((post) => {
+    post.data.tags?.forEach((tag) => {
+      tagMap.set(slugify(tag), tag)
+    })
+  })
+  return Array.from(tagMap).sort((a, b) => a[0].localeCompare(b[0]))
+}
 
-export async function getAllProjects(
+export async function getCategories() {
+  const posts = await getPosts()
+  const categoryMap = new Map()
+  posts.forEach((post) => {
+    const category = post.data.category || 'uncategorized'
+    categoryMap.set(slugify(category), category)
+  })
+  return Array.from(categoryMap).sort((a, b) => a[0].localeCompare(b[0]))
+}
+
+export async function getProjects(options) {
   options = {
-    chronSort: true,
-  },
-) {
-  let projects = await getCollection('projects')
+    ...{
+      chronSort: true,
+    },
+    ...options,
+  }
+  let projects = await getCollection('projects', ({ data }) =>
+    PROD ? data.draft !== true : true,
+  )
   if (options.chronSort)
     projects.sort(
-      (a, b) => b.data.dateStarted.valueOf() - a.data.dateStarted.valueOf(),
+      (a, b) => b.data.started_on.valueOf() - a.data.started_on.valueOf(),
     )
   return projects
 }
